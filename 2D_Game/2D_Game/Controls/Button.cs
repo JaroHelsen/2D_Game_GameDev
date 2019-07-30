4b92e6d77a880de25ab3b7322ec14bbf9c0d0a90
jaro <jaro.helsen@hotmail.be>
2019-07-30 17:34:56 +0200

    Update Button.cs
    finished button

--- 2D_Game/2D_Game/Controls/Button.cs
+++ 2D_Game/2D_Game/Controls/Button.cs
@@ -5,19 +5,79 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace _2D_Game.Controls
 {
     class Button : Component
     {
+        #region Fields
+        private MouseState currentMouse;
+        private MouseState previousMouse;
+        private SpriteFont font;
+        private Texture2D texture;
+        private bool isHovering;
+        #endregion
+
+        #region Properties
+        public event EventHandler Click;
+        public Boolean Clicked { get; set; }
+        public Color PenColour { get; set; }
+        public Vector2 Position { get; set; }
+        public String Text { get; set; }
+        public Rectangle Rectangle
+        {
+            get
+            {
+                return new Rectangle((int)Position.X, (int)Position.Y, texture.Width, texture.Height);
+            }
+        }
+        #endregion
+
+        public Button(Texture2D _texture, SpriteFont _font)
+        {
+            texture = _texture;
+            font = _font;
+            PenColour = Color.Black;
+        }
+
+
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            throw new NotImplementedException();
+            var colour = Color.White;
+
+            if (isHovering)
+                colour = Color.Gray;
+
+            spriteBatch.Draw(texture, Rectangle, colour);
+
+            if (!string.IsNullOrEmpty(Text))
+            {
+                var x = (Rectangle.X + (Rectangle.Width / 2)) - (font.MeasureString(Text).X / 2);
+                var y = (Rectangle.Y + (Rectangle.Height / 2)) - (font.MeasureString(Text).Y / 2);
+
+                spriteBatch.DrawString(font, Text, new Vector2(x, y), PenColour);
+            }
         }
 
         public override void Update(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            previousMouse = currentMouse;
+            currentMouse = Mouse.GetState();
+
+            var mouseRectangle = new Rectangle(currentMouse.X, currentMouse.Y, 1, 1);
+
+            isHovering = false;
+
+            if (mouseRectangle.Intersects(Rectangle))
+            {
+                isHovering = true;
+
+                if (currentMouse.LeftButton == ButtonState.Released && previousMouse.LeftButton == ButtonState.Pressed)
+                {
+                    Click?.Invoke(this, new EventArgs());
+                }
+            }
         }
     }
 }
