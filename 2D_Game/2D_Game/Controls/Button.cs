4732518eb1be2459f39fbb5d23fbd1f1e459ae75
jaro <jaro.helsen@hotmail.be>
2019-07-30 17:28:50 +0200

    implemented abstract class component

--- 2D_Game/2D_Game/Controls/Button.cs
+++ 2D_Game/2D_Game/Controls/Button.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace _2D_Game.Controls
 {
-    public abstract class Button: Component
+    class Button : Component
     {
-        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
-        public abstract void Update(GameTime gameTime);
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
