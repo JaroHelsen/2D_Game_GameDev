6b6bc92fdfb98bf0a141a81fe6e7c16a740a2ce0
jaro <jaro.helsen@hotmail.be>
2019-07-31 16:38:20 +0200

    Updated controls

--- 2D_Game/2D_Game/Controls/Button.cs
+++ 2D_Game/2D_Game/Controls/Button.cs
@@ -12,68 +12,78 @@ namespace _2D_Game.Controls
     class Button : Component
     {
         #region Fields
-        private MouseState currentMouse;
-        private MouseState previousMouse;
-        private SpriteFont font;
-        private Texture2D texture;
-        private bool isHovering;
+        private MouseState _currentMouse;
+        private MouseState _previousMouse;
+        private SpriteFont _font;
+        private Texture2D _texture;
+        private bool _isHovering;
         #endregion
 
         #region Properties
         public event EventHandler Click;
-        public Boolean Clicked { get; set; }
+        public bool Clicked { get; set; }
         public Color PenColour { get; set; }
         public Vector2 Position { get; set; }
-        public String Text { get; set; }
         public Rectangle Rectangle
         {
             get
             {
-                return new Rectangle((int)Position.X, (int)Position.Y, texture.Width, texture.Height);
+                return new Rectangle((int)Position.X, (int)Position.Y, _texture.Width, _texture.Height);
             }
+        }
+        public string Text { get; set; }
+        #endregion
+
+        #region Constructor
+        public Button(Texture2D texture, SpriteFont font)
+        {
+            _texture = texture;
+            _font = font;
+            PenColour = Color.Black;
         }
         #endregion
 
-        public Button(Texture2D _texture, SpriteFont _font)
-        {
-            texture = _texture;
-            font = _font;
-            PenColour = Color.Black;
-        }
-
-
+        /// <summary>
+        /// Draw the button
+        /// </summary>
+        /// <param name="gameTime"></param>
+        /// <param name="spriteBatch"></param>
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             var colour = Color.White;
 
-            if (isHovering)
+            if (_isHovering)
                 colour = Color.Gray;
 
-            spriteBatch.Draw(texture, Rectangle, colour);
+            spriteBatch.Draw(_texture, Rectangle, colour);
 
             if (!string.IsNullOrEmpty(Text))
             {
-                var x = (Rectangle.X + (Rectangle.Width / 2)) - (font.MeasureString(Text).X / 2);
-                var y = (Rectangle.Y + (Rectangle.Height / 2)) - (font.MeasureString(Text).Y / 2);
+                var x = (Rectangle.X + (Rectangle.Width / 2)) - (_font.MeasureString(Text).X / 2);
+                var y = (Rectangle.Y + (Rectangle.Height / 2)) - (_font.MeasureString(Text).Y / 2);
 
-                spriteBatch.DrawString(font, Text, new Vector2(x, y), PenColour);
+                spriteBatch.DrawString(_font, Text, new Vector2(x, y), PenColour);
             }
         }
 
+        /// <summary>
+        /// Update the button state and check if the mouse is hovering over the button.
+        /// </summary>
+        /// <param name="gameTime"></param>
         public override void Update(GameTime gameTime)
         {
-            previousMouse = currentMouse;
-            currentMouse = Mouse.GetState();
+            _previousMouse = _currentMouse;
+            _currentMouse = Mouse.GetState();
 
-            var mouseRectangle = new Rectangle(currentMouse.X, currentMouse.Y, 1, 1);
+            var mouseRectangle = new Rectangle(_currentMouse.X, _currentMouse.Y, 1, 1);
 
-            isHovering = false;
+            _isHovering = false;
 
             if (mouseRectangle.Intersects(Rectangle))
             {
-                isHovering = true;
+                _isHovering = true;
 
-                if (currentMouse.LeftButton == ButtonState.Released && previousMouse.LeftButton == ButtonState.Pressed)
+                if (_currentMouse.LeftButton == ButtonState.Released && _previousMouse.LeftButton == ButtonState.Pressed)
                 {
                     Click?.Invoke(this, new EventArgs());
                 }
--- 2D_Game/2D_Game/Controls/Controls.cs
+++ 2D_Game/2D_Game/Controls/Controls.cs
@@ -9,62 +9,51 @@ namespace _2D_Game.Controls
 {
     public abstract class Controls
     {
-        public Boolean Left { get; set; }
-        public Boolean Right { get; set; }
-        public Boolean Jump { get; set; }
+        /// <summary>
+        /// Abstract class so we can use other input methods eventually.
+        /// </summary>
+        public bool Left { get; set; }
+        public bool Right { get; set; }
+        public bool Jump { get; set; }
         public abstract void Update();
     }
 
-    public class BedieningPijltjes: Controls
+    public class BedieningPijltjes : Controls
     {
+        /// <summary>
+        /// Updates the input.
+        /// Checks the key state and sets a boolean to true/false/nothing depending on if the if was true or not.
+        /// You cannot push the left and the right button at once.
+        /// </summary>
         public override void Update()
         {
             KeyboardState stateKey = Keyboard.GetState();
-            //bool isIdle = true;
             Console.WriteLine("update input");
-            if (stateKey.IsKeyDown(Keys.Left))
+            if (stateKey.IsKeyDown(Keys.Left) && Right == false)
             {
                 Left = true;
-                //isIdle = false;
             }
             if (stateKey.IsKeyUp(Keys.Left))
             {
                 Left = false;
-                //isIdle = true;
             }
 
-            if (stateKey.IsKeyDown(Keys.Right))
+            if (stateKey.IsKeyDown(Keys.Right) && Left == false)
             {
                 Right = true;
-                //isIdle = false;
             }
             if (stateKey.IsKeyUp(Keys.Right))
             {
                 Right = false;
-                //isIdle = true;
             }
             if (stateKey.IsKeyDown(Keys.Up))
             {
                 Jump = true;
-                //isIdle = false;
             }
             if (stateKey.IsKeyUp(Keys.Up))
             {
                 Jump = false;
-                //isIdle = true;
             }
-            //if (Left == false && Right == false && Jump == false)
-            //{
-            //    isIdle = true;
-            //}
-            //else
-            //{
-            //    isIdle = false;
-            //}
-            //if (isIdle == true /*&& stateKey.GetPressedKeys() == null || stateKey.GetPressedKeys().Length == 0*/)
-            //{
-            //    Idle = true;
-            //}
         }
     }
 }
