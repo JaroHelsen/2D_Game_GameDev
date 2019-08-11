2f94518fbce08fc4272e0352e3052bec841adf3b
jaro <jaro.helsen@hotmail.be>
2019-08-11 18:24:15 +0200

    Update Enemies.cs
    deleted excess code and generalised colrect

--- 2D_Game/2D_Game/MovingSprites/Enemies.cs
+++ 2D_Game/2D_Game/MovingSprites/Enemies.cs
@@ -11,13 +11,13 @@ namespace _2D_Game.MovingSprites
     class Enemies: Sprite
     {
 
-        private Rectangle collisionRectangle;
+        //private Rectangle collisionRectangle;
 
-        public Rectangle CollisionRectangle
-        {
-            get { return collisionRectangle; }
-            set { collisionRectangle = value; }
-        }
+        //public Rectangle CollisionRectangle
+        //{
+        //    get { return collisionRectangle; }
+        //    set { collisionRectangle = value; }
+        //}
 
         Texture2D texture;
         Rectangle rectangle;
@@ -95,53 +95,5 @@ namespace _2D_Game.MovingSprites
                 spriteBatch.Draw(SpriteTexture, Position, null, Color.White, rotation, origin, 1f, SpriteEffects.None, 0f);
             }
         }
-        //public bool isVisible = false;
-        //Random random = new Random();
-        //int randX;
-        //private Vector2 startPosition;
-
-
-        ///// <summary>
-        ///// Constructor for the Enemies
-        ///// </summary>
-        ///// <param name="_position"></param>
-        ///// <param name="_texture"></param>
-        //public Enemies(Vector2 _position, Texture2D _texture) : base(_position)
-        //{
-        //    SpriteTexture = _texture;
-        //    startPosition = _position;
-        //    randX = random.Next(-4, -1);
-        //    Velocity = new Vector2(randX, 0);
-        //}
-
-        ///// <summary>
-        ///// Updating the movement so the enemy can move left and right for a set amount of paces.
-        ///// </summary>
-        ///// <param name="gameTime"></param>
-        //public override void Update(GameTime gameTime)
-        //{
-        //    Position += Velocity;
-        //    //if (Position.Y <=0 || Position.Y >= Game1.screenHeight -  SpriteTexture.Height)
-        //    //{
-        //    //    Velocity.Y = -Velocity.Y;
-        //    //}
-        //    if (Position.X > startPosition.X + 20)
-        //    {
-        //        Position.X = -Position.X;
-        //    }
-        //    else if (Position.X < startPosition.X + 20)
-        //    {
-        //        Position.X = -Position.X;
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Drawing the enemy.
-        ///// </summary>
-        ///// <param name="spriteBatch"></param>
-        //public override void Draw(SpriteBatch spriteBatch)
-        //{
-        //    spriteBatch.Draw(SpriteTexture, Position, Color.AliceBlue);
-        //}
     }
 }
