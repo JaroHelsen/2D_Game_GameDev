ad954e6bbab10195bf8e04d7aec9b0fb900f1cda
jaro <jaro.helsen@hotmail.be>
2019-08-18 22:33:20 +0200

    Update LevelFactory.cs
    updated the end sign of a level

--- 2D_Game/2D_Game/LevelDesign/LevelFactory.cs
+++ 2D_Game/2D_Game/LevelDesign/LevelFactory.cs
@@ -56,7 +56,7 @@ namespace _2D_Game.LevelDesign
                     }
                     else if (tileArray[i, j] == 25)
                     {
-                        blokArray[i, j] = new Blok(content.Load<Texture2D>("Objects/Crate"), new Vector2(128 * i, 128 * j));
+                        blokArray[i, j] = new Blok(content.Load<Texture2D>("Objects/Sign"), new Vector2(128 * i, 128 * j + 50));
                         blokArray[i, j].Id = 25;
                     }
                     else
@@ -67,67 +67,9 @@ namespace _2D_Game.LevelDesign
             }
         }
 
-        //public void DestroyLevel()
-        //{
-        //    Array.Clear(blokArray, 0, blokArray.Length);
-        //    Array.Clear(tileArray, 0, tileArray.Length);
-        //    enemies.Clear();
-
-        //}
-
         public abstract void DrawWorld(SpriteBatch spriteBatch);
-        //public void DrawWorld(SpriteBatch spriteBatch)
-        //{
-        //    for (int i = 0; i < tileArray.GetLength(0); i++)
-        //    {
-        //        for (int j = 0; j < tileArray.GetLength(1); j++)
-        //        {
-        //            if (blokArray[i, j] != null)
-        //            {
-        //                blokArray[i, j].Draw(spriteBatch);
-        //            }
-        //        }
-        //    }
-        //    foreach (Enemies enemy in enemies)
-        //    {
-        //        enemy.Draw(spriteBatch);
-        //    }
-        //}
 
         public abstract void CheckForCollision(GameTime gameTime, Hero hero, ContentManager content);
-        //public void CheckForCollision(GameTime gameTime, Hero hero, ContentManager content)
-        //{
-        //    hero.Update(gameTime);
-        //    foreach (Enemies enemy in enemies)
-        //    {
-        //        enemy.Update(gameTime);
-        //    }
-
-        //    heroCollisionChecker.CheckCollision();
-        //    enemyCollisionChecker.CheckCollision();
-
-        //    for (int i = enemies.Count -  1 ; i >= 0 ; i--)
-        //    {
-        //        if (enemies[i].Health == 0)
-        //        {
-        //            enemies[i].Position.X = -1000;
-        //        }
-        //    }
-        //    foreach (Blok blok in blokArray)
-        //    {
-        //        if (blok != null)
-        //        {
-        //            if (blok.FinishLine)
-        //            {
-        //                Console.WriteLine("This is the end");
-        //                Console.WriteLine("-------------------------------------------------------------------------");
-        //                Console.WriteLine("");
-        //                EndOfLevel(content);
-        //            }
-        //        }
-
-        //    }
-        //}
 
         public abstract void ResetLevel();
 
